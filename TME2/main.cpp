f51b1fa560f7ff705a08960971c63e7913b302f9
TO Jeremy <jeremy.to@etu.sorbonne-universite.fr>
2023-10-01 18:04:42 +0200

    Question 3

--- TME2/main.cpp
+++ TME2/main.cpp
@@ -3,6 +3,7 @@
 #include <regex>
 #include <chrono>
 #include <vector>
+#include <unordered_map> // Pour la question 2
 
 int main()
 {
@@ -14,22 +15,27 @@ int main()
     auto start = steady_clock::now();
     cout << "Parsing War and Peace" << endl;
 
-    vector<pair<string, size_t>> freq;
+    vector<pair<string, size_t>> freq;       // Pour la question 3
+    unordered_map<string, size_t> wordCount; // Pour la question 2
 
     size_t nombre_lu = 0;
     // prochain mot lu
     string word;
-    // une regex qui reconnait les caractères anormaux (négation des lettres)
+    // une regex qui reconnaît les caractères anormaux (négation des lettres)
     regex re(R"([^a-zA-Z])");
     while (input >> word)
     {
-        bool trouve = false;
-
         // élimine la ponctuation et les caractères spéciaux
         word = regex_replace(word, re, "");
-        // passe en lowercase
+        // passe en minuscules
         transform(word.begin(), word.end(), word.begin(), ::tolower);
 
+        // Question 2 : Compter le nombre de mots différents
+        wordCount[word]++;
+
+        bool trouve = false;
+
+        // Question 3 : Calculer le nombre d'occurrences de chaque mot
         for (auto &pair : freq)
         {
             if (pair.first == word)
@@ -55,12 +61,23 @@ int main()
     cout << "Finished Parsing War and Peace" << endl;
 
     auto end = steady_clock::now();
-    cout << "Parsing took "
-         << duration_cast<milliseconds>(end - start).count()
-         << "ms.\n";
+    cout << "Parsing took " << duration_cast<milliseconds>(end - start).count() << "ms.\n";
 
-    cout << "Found a total of " << nombre_lu << " words." << endl;
+    // Question 2 : Afficher le nombre de mots différents
+    cout << "Found a total of " << wordCount.size() << " different words." << endl;
+
+    // Question 3 : Afficher le nombre d'occurrences des mots spécifiques
+    for (const string &targetWord : {"war", "peace", "toto"})
+    {
+        for (const auto &pair : freq)
+        {
+            if (pair.first == targetWord)
+            {
+                cout << "Occurrences of \"" << targetWord << "\": " << pair.second << endl;
+                break;
+            }
+        }
+    }
 
     return 0;
 }
-
