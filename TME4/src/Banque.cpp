f87726518fb8edc42d21e560628bc63d12f4ef18
TO Jeremy <jerem.to@etu.sorbonne-universite.fr>
2023-10-19 23:27:10 +0200

    QUestion 4 TME3

--- TME4/src/Banque.cpp
+++ TME4/src/Banque.cpp
@@ -9,9 +9,16 @@ namespace pr {
 void Banque::transfert(size_t deb, size_t cred, unsigned int val) {
 	Compte & debiteur = comptes[deb];
 	Compte & crediteur = comptes[cred];
+
+	debiteur.lock(); //Question 4 TME3
+	crediteur.lock(); //Question 4 TME3
+
 	if (debiteur.debiter(val)) {
 		crediteur.crediter(val);
 	}
+
+	debiteur.unlock(); //Question 4 TME3
+	crediteur.unlock(); //Question 4 TME3
 }
 size_t Banque::size() const {
 	return comptes.size();
