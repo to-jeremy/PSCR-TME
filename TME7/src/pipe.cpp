d13069a781613e3ac765cb5e82ccc3a8a683d7e2
TO Jeremy <jerem.to@etu.sorbonne-universite.fr>
2023-12-07 22:18:21 +0100

    Question 1 TME 7

--- TME7/src/pipe.cpp
+++ TME7/src/pipe.cpp
@@ -1,26 +1,109 @@
-#include <cstdlib>
-#include <cstdio>
-#include <cstddef>
-#include <ostream>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <cstring>
 #include <iostream>
 #include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <fcntl.h>
-#include <sys/wait.h>
+#include <fcntl.h> //Partie Sémaphores
+#include <sys/stat.h> //Partie Sémaphores
+#include <semaphore.h> //Partie Sémaphores
 
 using namespace std;
 
 
 //Partie 1 : Fork, exec, pipe
 //Question 1
-int main() {
+
+void exec (int argc, chat ** args) {
+    cout << "Args :";
+    for (int i=0; i < argc; i++) {
+        if (args[i] != nullptr) {
+            cout << args[i] << " ";
+        } else {
+            break;
+        }
+    }
+    cout << endl;
+
+    if (execv(agrs[0], args) == -1) {
+        perror("exec");
+        exit(3);
+    }
+}
+
+int main (int argc, char ** argv) {
+
+    char * args1[argc];
+    char * args2[argc];
+
+    memset(args1, 0, argc * sizeof(char *));
+    memset(args2, 0, argc * sizeof(char *));
+
+    int arg = 1;
+
+    for ( ; arg < argc; arg++) {
+        if (!strcmp(argv[arg], "|")) {
+            arg++;
+            break;
+        } else {
+            args[arg -1] = argv[arg];
+        }
+    }
+
+    for (int i=0; arg < argc; i++; arg++) {
+        args2[i] = argv[arg];
+    }
+
+    int tube_principal[2];
+    pid_t p_fils;
+
+    if (pipe(tube_principal) == -1) {
+        perror("pipe");
+        exit(1);
+    }
+
+    if ((p_fils = fork()) == -1) {
+        perror("fork");
+        exit(2);
+    }
+
+    if (p_fils == 0) { /*fils 1*/
+        dup2(tube_principal[1], STDOUT_FILENO);
+        close(tube_principal[1]);
+        close(tube_principal[0]);
+
+        exec(argc, args1);
+    }
+
+    if ((p_fils = fork()) == -1) {
+        perror("fork");
+        exit(2);
+    }
+
+    if (p_fils == 0) { /*fils 2*/
+        dup2(tube_principal[0], STDOUT_FILENO);
+        close(tube_principal[0]);
+        close(tube_principal[1]);
+
+        exec(argc, args2);
+    }
+
+    close(tube_principal[0]);
+    close(tube_principal(1));
+
+    wait(0);
+    wait(0);
+
+    return 0;
+
+}
+
+/*int main() {
 
     pid_t p, f1, f2;
     p=getpid();
     int pfd[2];
-
-    //execv ("pipe", "/bin/cat", "pipe.cpp", "\|", "/bin/wc", "-l");
 
     if(pipe(pfd)) {
         perror("Pipe");
@@ -59,15 +142,7 @@ int main() {
 
     int pos=-1;
 
-}
-
-//int execv (const char *path, char * const argv[]) {}
-
-int main_execv (int argc, char **argv[]) {
-    execv ("/bin/cat", "pipe.cpp", "\|", "/bin/wc", "-l");
-    perror("execv");
-    return EXIT_SUCCESS;
-}
+}*/
 
 //Fin question 1
 
@@ -76,11 +151,12 @@ int main_execv (int argc, char **argv[]) {
 
 /*Détails persos à connaitre*/
 
-//descripteur
-//entrée : 0
-//sortie : 1
-//sortie erreur : 2
+/*descripteur
+entrée : 0
+sortie : 1
+sortie erreur : 2
 
-//wc -w : renvoie le nombre de mots dans un fichier
-//wc -l : renvoie le nombre de lignes
-//wc -c : le nombre de caractères
+wc -w : renvoie le nombre de mots dans un fichier
+wc -l : renvoie le nombre de lignes
+wc -c : le nombre de caractères*/
+
